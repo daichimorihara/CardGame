08576b88e6a1f118ec1f44ac695100b40d5a9fe5
Daichi Morihara <daichi16daichi04@icloud.com>
2021-10-08 21:33:54 +0900

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Daichi Morihara on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
